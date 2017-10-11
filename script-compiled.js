a1897c7b98c903064ebd8928e404449373fcd537
Bartłomiej Jakubowski <bartlomiej-jakubowski@wp.pl>
2017-10-11 20:08:58 +0200

    ES6 js code

---
+++ script-compiled.js
@@ -0,0 +1,40 @@
+'use strict';
+
+var _templateObject = _taggedTemplateLiteral(['', ''], ['', '']);
+
+function _taggedTemplateLiteral(strings, raw) { return Object.freeze(Object.defineProperties(strings, { raw: { value: Object.freeze(raw) } })); }
+
+//Zadanie 1
+var x = 'Hello';
+var y = 'World!';
+console.log(('' + x)(_templateObject, y));
+
+//Zadanie 2
+var multiply = function multiply(a) {
+  var b = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 1;
+  return a * b;
+};
+console.log(multiply(5, 2));
+
+//Zadanie 3
+var average = function average() {
+  for (var _len = arguments.length, nums = Array(_len), _key = 0; _key < _len; _key++) {
+    nums[_key] = arguments[_key];
+  }
+
+  return nums.reduce(function (a, b) {
+    return a + b;
+  }) / nums.length;
+};
+console.log(average(2, 3, 4));
+
+//Zadanie 4
+var grades = [1, 5, 5, 5, 4, 3, 3, 2, 1];
+console.log(average.apply(undefined, grades));
+
+//Zadanie 5
+var weird = [1, 4, 'Iwona', false, 'Nowak'];
+var second = weird[2],
+    fourth = weird[4];
+
+console.log(second + ' ' + fourth);
---
+++ script.js
@@ -0,0 +1,21 @@
+//Zadanie 1
+const x = 'Hello';
+const y = 'World!';
+console.log(`${x}` `${y}`);
+
+//Zadanie 2
+const multiply = (a, b = 1) => a*b;
+console.log(multiply(5, 2));
+
+//Zadanie 3
+const average = (...nums) => nums.reduce((a,b) => a+b)/nums.length;
+console.log(average(2, 3, 4));
+
+//Zadanie 4
+const grades = [1, 5, 5, 5, 4, 3, 3, 2, 1];
+console.log(average(...grades));
+
+//Zadanie 5
+const weird = [1, 4, 'Iwona', false, 'Nowak'];
+const [, ,second, ,fourth] = weird;
+console.log(`${second} ${fourth}`);
